bbb611c77b842cc192060b17c2bad39caa6694c7
heedong12 <dlgmldus3289@naver.com>
2024-06-24 23:19:27 +0900

    feat: add festino title

--- src/components/tablings/Tabling.vue
+++ src/components/tablings/Tabling.vue
@@ -6,18 +6,13 @@ import TablingTabs from '@/components/tablings/TablingTabs.vue';
 <template>
   <TablingBanner />
   <TablingTabs />
-
-  <div class="grid grid-cols-2 gap-4 pt-16 w-full dynamic-padding">
-    <!-- TODO: Make to Component -->
-    <div v-for="item in 4" class="dynamic-item bg-red-200"></div>
-  </div>
 </template>
 
 <style lang="css" scoped>
 .dynamic-item {
   min-width: 100px;
   min-height: 100px;
-  width: calc(190 / 430 * 100%) !important;
+  width: calc(190 / 390 * 100%) !important;
   height: calc(190 / 430 * 100%) !important;
 }
 
