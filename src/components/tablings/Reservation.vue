0c8e3fa4931fef8f1644355117bd21937cc77bcd
heedong12 <dlgmldus3289@naver.com>
2024-06-25 00:13:35 +0900

    feat: create tabling component

---
+++ src/components/tablings/Reservation.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex flex-col dynamic-padding pt-16 gap-2.5">
+    <div class="flex flex-row gap-2.5">
+      <div v-for="item in 2" class="bg-red-50 dynamic-item rounded-3xl"></div>
+    </div>
+    <div class="flex flex-row gap-2.5">
+      <div v-for="item in 2" class="bg-red-50 dynamic-item rounded-3xl"></div>
+    </div>
+  </div>
+  <div class="flex flex-row dynamic-padding justify-between gap-5 text-white font-bold mt-5">
+    <button class="bg-secondary-100 h-[54px] rounded-xl w-full">자세히보기</button>
+    <button class="bg-secondary-100 h-[54px] rounded-xl w-full">예약하기</button>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="css" scoped>
+.dynamic-item {
+  min-width: 100px;
+  min-height: 100px;
+  width: calc(190 / 390 * 100%) !important;
+  height: calc(190 / 430 * 100%) !important;
+}
+
+.dynamic-padding {
+  padding-left: calc(20 / 430 * 100%) !important;
+  padding-right: calc(20 / 430 * 100%) !important;
+}
+</style>
---
+++ src/components/tablings/SearchReservation.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { ref, watchEffect } from 'vue';
+
+const name = ref('');
+const phoneNum = ref('');
+const isInputFill = ref(false);
+
+watchEffect((name, phoneNum) => {
+  isInputFill.value = name.value && phoneNum.value;
+});
+</script>
+
+<template>
+  <div class="flex flex-col dynamic-padding pt-16 h-full">
+    <div class="flex flex-col gap-1.5 px-4">
+      <div class="text-xs">이름</div>
+      <div class="h-11 w-full flex flex-row border-b-1 border-secondary-500 items-center py-2.5 gap-2.5">
+        <div class="h-6 w-6 bg-red-50"></div>
+        <input class="text-secondary-100 flex-1 focus:outline-none" type="text" placeholder="홍길동" v-model="name" />
+      </div>
+    </div>
+    <div class="flex flex-col gap-1.5 px-4 pt-[30px]">
+      <div class="text-xs">전화번호</div>
+      <div class="h-11 w-full flex flex-row border-b-1 border-secondary-500 items-center py-2.5 gap-2.5">
+        <div class="h-6 w-6 bg-red-50"></div>
+        <input
+          class="text-secondary-100 flex-1 focus:outline-none"
+          type="number"
+          placeholder="010-1234-5678"
+          v-model="phoneNum"
+        />
+      </div>
+    </div>
+    <div class="flex-1"></div>
+
+    <button
+      class="mx-5 h-[54px] bg-secondary-100 text-white font-bold rounded-xl"
+      :class="{ 'bg-primary-900': isInputFill }"
+    >
+      조회하기
+    </button>
+  </div>
+</template>
+
+<style lang="css" scoped>
+.dynamic-padding {
+  padding-left: calc(20 / 430 * 100%) !important;
+  padding-right: calc(20 / 430 * 100%) !important;
+}
+</style>
