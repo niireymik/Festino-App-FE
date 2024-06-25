15bef087fafa19502b1bc0f664b06945ddfdb556
임정윤 <jeong.yun@tukorea.ac.kr>
2024-06-25 23:53:21 +0900

    feat : make notification view prototype

---
+++ src/views/NotificationView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col min-h-screen h-full items-center bg-[#FBFCFF]">
+    <Header />
+    <div class="flex flex-row w-full h-[48px] text-secondary-700 bg-white justify-center items-center font-medium text-xl">공지사항</div>
+    <div v-for="item in 3" :key="item" class="pt-5">
+      <Notice />
+    </div>
+  </div>
+</template>
+  
+<script setup>
+  import Notice from '@/components/notice/Notice.vue';
+  import Header from '@/components/header/Header.vue';
+</script>
+  
+<style lang="css" scoped></style>
+  
