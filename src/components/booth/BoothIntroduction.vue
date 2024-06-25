50898a6f95c26195cd0c77cbd21064eb9cdf7307
김민지 <alswlk0306@gmail.com>
2024-06-25 20:02:42 +0900

    feat: booth introduce component

---
+++ src/components/booth/BoothIntroduction.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="w-full h-40 flex flex-row justify-center items-center rounded-3.5xl border border-primary-300 px-4 py-3">
+      <div class="w-2/3 flex flex-col justify-center">
+          <div class="px-2 py-1 w-fit flex justify-center text-center rounded-3.5xl border border-primary-700 text-primary-700 text-3xs font-pretendard font-semibold">#푸드트럭</div>
+          <div class="py-1.5 text-base font-pretendard font-semibold">치즈닭갈비</div>
+          <div class="pb-3 text-2xs text-secondary-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+          <div class="flex flex-row">
+            <div class="px-2 py-1 mr-2 w-fit flex justify-center text-center text-3xs text-white bg-primary-700 rounded-full">운영중</div>
+            <div class="px-2 py-1 w-fit flex justify-center text-center items-center text-3xs text-secondary-500 bg-primary-100 rounded-full">
+              <img class="mr-1 w-2 h-2 flex justify-center" src="/images/booth-clock-icon.png" />
+              <div>12:00 ~ 16:00</div>
+            </div>
+          </div>
+      </div>
+      <div class="w-32 h-32 flex justify-center items-center">
+        <div class="w-full h-full bg-primary-300 rounded-3.5xl"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="">
+</style>
