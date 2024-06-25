3f5f6b51b4777ba86926aa31ccd7f146a5bc6292
김민지 <alswlk0306@gmail.com>
2024-06-26 04:55:38 +0900

    feat: booth page banner

---
+++ src/components/booth/BoothBanner.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="relative">
+    <div class="w-full min-h-[240px] bg-booth-banner">
+      <div class="absolute w-auto h-auto dynamic-top dynamic-padding">
+        <div class="banner-title-shadow bg-gradient-to-b from-white from-50% to-primary-300 bg-clip-text text-transparent font-jalnan2 text-md">티노와 함께</div>
+        <div class="banner-title-shadow bg-gradient-to-b from-white from-50% to-primary-300 bg-clip-text text-transparent font-jalnan2 text-3xl">축제 부스 알아보기</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="css" scoped>
+.dynamic-top {
+  top: calc(85 / 230 * 100%) !important;
+}
+.dynamic-padding {
+  padding-left: calc(20 / 430 * 100%) !important;
+  padding-right: calc(20 / 430 * 100%) !important;
+}
+.banner-title-shadow {
+  text-shadow: 0 4px 3px rgba(0, 0, 0, 0.15);
+}
+</style>
---
+++ src/views/BoothView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <BoothBanner />
+    <div v-for="i in 5" :key="i">
+      <BoothIntro />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import BoothBanner from '@/components/booth/BoothBanner.vue';
+import BoothIntro from '@/components/booth/BoothIntroduction.vue';
+</script>
+
+<style lang="">
+    
+</style>
