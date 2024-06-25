a67f3d38465911163126da4835c243b330539f02
김민지 <alswlk0306@gmail.com>
2024-06-26 05:21:22 +0900

    style: modify text and border color

--- src/components/booth/BoothIntroduction.vue
+++ src/components/booth/BoothIntroduction.vue
@@ -2,11 +2,11 @@
   <div class="dynamic-padding pb-2">
     <div class="w-full h-40 shadow-3xl flex flex-row justify-between items-center rounded-3.5xl border border-primary-900-light px-4 py-3">
       <div class="w-[222px] flex flex-col justify-center">
-          <div class="px-2 py-1 w-fit flex justify-center text-center rounded-3.5xl border border-primary-900 text-primary-700 text-3xs font-pretendard font-semibold">#푸드트럭</div>
+          <div class="px-2 py-1 w-fit flex justify-center text-center rounded-3.5xl border border-primary-900 text-primary-900 text-3xs font-pretendard font-semibold">#푸드트럭</div>
           <div class="py-1.5 text-base font-pretendard font-semibold">치즈닭갈비</div>
           <div class="pb-2 text-2xs text-secondary-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
           <div class="flex flex-row">
-            <div class="px-2 py-1 mr-2 w-fit flex justify-center text-center text-3xs text-white bg-primary-700 rounded-full">운영중</div>
+            <div class="px-2 py-1 mr-2 w-fit flex justify-center text-center text-3xs text-white bg-primary-900 rounded-full">운영중</div>
             <div class="px-2 py-1 w-fit flex justify-center text-center items-center text-3xs text-secondary-500 bg-primary-100 rounded-full">
               <img class="mr-1 w-2 h-2 flex justify-center" src="/images/booth-clock-icon.png" />
               <div>12:00 ~ 16:00</div>
