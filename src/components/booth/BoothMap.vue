f0b5a2b1eff1949378f8c89aa16acf0f8a19e6c8
김민지 <alswlk0306@gmail.com>
2024-06-26 05:56:11 +0900

    feat: map zone

---
+++ src/components/booth/BoothMap.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="dynamic-padding w-full h-auto">
+    <div class="w-[388px] h-[382px] bg-primary-300 border border-primary-900-light rounded-3xl"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="css" scoped>
+.dynamic-padding {
+  padding-top: calc(25 / 430 * 100%) !important;
+  padding-left: calc(20 / 430 * 100%) !important;
+  padding-right: calc(20 / 430 * 100%) !important;
+  padding-bottom: calc(25 / 430 * 100%) !important;
+}
+</style>
