cd978ae5083e37e1dfc57c66898f4cfc749c8d60
김민지 <alswlk0306@gmail.com>
2024-06-26 05:22:08 +0900

    feat: add festino icon

--- src/components/booth/BoothBanner.vue
+++ src/components/booth/BoothBanner.vue
@@ -2,6 +2,7 @@
   <div class="relative">
     <div class="w-full min-h-[240px] bg-booth-banner">
       <div class="absolute w-auto h-auto dynamic-top dynamic-padding">
+        <div class="px-3 py-1 mb-2 w-fit flex justify-center text-center rounded-3.5xl border border-white text-white text-3xs font-pretendard">Festino</div>
         <div class="banner-title-shadow bg-gradient-to-b from-white from-50% to-primary-300 bg-clip-text text-transparent font-jalnan2 text-md">티노와 함께</div>
         <div class="banner-title-shadow bg-gradient-to-b from-white from-50% to-primary-300 bg-clip-text text-transparent font-jalnan2 text-3xl">축제 부스 알아보기</div>
       </div>
@@ -18,7 +19,7 @@ export default {
 
 <style lang="css" scoped>
 .dynamic-top {
-  top: calc(85 / 230 * 100%) !important;
+  top: calc(58 / 230 * 100%) !important;
 }
 .dynamic-padding {
   padding-left: calc(20 / 430 * 100%) !important;
