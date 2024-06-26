841b1883e6d535546456adda4656e61e64b96db9
heedong12 <dlgmldus3289@naver.com>
2024-06-27 04:11:54 +0900

    feat: add font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,6 +35,7 @@ export default {
         '3xs': '0.5rem', // 8px
         '2xs': '0.625rem', // 10px
         '3xl': '1.75rem', // 28px
+        '10xl': '6.25rem', //100px
       },
       fontFamily: {
         pretendard: ['Pretendard Variable'],
