7cc742321fdcb9783c6ff2657b10715a15c9a5c9
김민지 <alswlk0306@gmail.com>
2024-06-26 18:14:35 +0900

    feat: map zoom in, zoom out button

--- src/components/booth/BoothMap.vue
+++ src/components/booth/BoothMap.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="dynamic-padding w-full h-auto">
-    <div class="w-full min-h-[382px] bg-primary-300 border border-primary-900-light rounded-3xl"></div>
+    <div class="relative w-full min-h-[382px] bg-primary-300 border border-primary-900-light rounded-3xl">
+      <div class="absolute bottom-5 left-5">
+        <button class="shadow-3xl bg-white rounded-t-full border-1 border-primary-900-light p-4 flex justify-center items-center">
+          <i class="pi pi-plus text-primary-900 text-lg"></i>
+        </button>
+        <button class="shadow-3xl bg-white rounded-b-full border-1 border-primary-900-light p-4 flex justify-center items-center">
+          <i class="pi pi-minus text-primary-900 text-lg"></i>
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
