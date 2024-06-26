57c0bb00e283c50ca9748a4cca86f64931bbb4af
heedong12 <dlgmldus3289@naver.com>
2024-06-27 00:10:57 +0900

    feat: change color when icon clicked

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,16 +1,35 @@
 <script setup>
+import Home from './footers/HomeIcon.vue';
+import TimeTableIcon from './footers/TimeTableIcon.vue';
+import BoothIcon from './footers/BoothIcon.vue';
+import TablingIcon from './footers/TablingIcon.vue';
+import { ref } from 'vue';
+
 const ICON_URL_MAP = [
-  { name: '홈', url: '/icons/help_clinic.svg', width: '28px' },
-  { name: '타임테이블', url: '/icons/acute.svg', width: '44px' },
-  { name: '부스', url: '/icons/distance.svg', width: '28px' },
-  { name: '테이블링', url: '/icons/hourglass_bottom.svg', width: '35px' },
+  { name: '홈', component: Home, width: '28px' },
+  { name: '타임테이블', component: TimeTableIcon, url: '/icons/acute.svg', width: '44px' },
+  { name: '부스', component: BoothIcon, url: '/icons/distance.svg', width: '28px' },
+  { name: '테이블링', component: TablingIcon, url: '/icons/hourglass_bottom.svg', width: '35px' },
 ];
+
+const selectedFooterIndex = ref(-1);
+const handleClickFooter = (index) => {
+  selectedFooterIndex.value = index;
+  console.log(selectedFooterIndex.value);
+};
 </script>
 <template>
   <div class="w-full h-[60px] bg-white flex items-center justify-around fixed bottom-0">
-    <div class="flex flex-col items-center justify-center" v-for="item in ICON_URL_MAP" :class="`w-[${item.width}]`">
-      <img :src="item.url" class="w-6 h-6" />
-      <div class="text-2xs text-secondary-100">{{ item.name }}</div>
+    <div
+      class="flex flex-col items-center justify-center"
+      v-for="(item, index) in ICON_URL_MAP"
+      :class="`w-[${item.width}]`"
+      @click="handleClickFooter(index)"
+    >
+      <component :is="item.component" :isActive="index === selectedFooterIndex" />
+      <div class="text-2xs" :class="index === selectedFooterIndex ? 'text-primary-900' : 'text-secondary-100'">
+        {{ item.name }}
+      </div>
     </div>
   </div>
 </template>
