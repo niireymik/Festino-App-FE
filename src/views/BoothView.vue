3a8f8bed4549f89112d78c5a7d6c3b78a10a1415
김민지 <alswlk0306@gmail.com>
2024-06-26 15:34:36 +0900

    feat: modify booth page

--- src/views/BoothView.vue
+++ src/views/BoothView.vue
@@ -1,17 +1,25 @@
 <template>
   <div>
     <BoothBanner />
-    <div v-for="i in 5" :key="i">
-      <BoothIntro />
+    <BoothMap />
+    <div class="dynamic-padding">
+      <BoothMenu />
     </div>
+    <BoothIntro />
   </div>
 </template>
 
 <script setup>
 import BoothBanner from '@/components/booth/BoothBanner.vue';
+import BoothMap from '@/components/booth/BoothMap.vue';
+import BoothMenu from '@/components/booth/BoothMenu.vue';
 import BoothIntro from '@/components/booth/BoothIntroduction.vue';
 </script>
 
-<style lang="">
-    
+<style lang="css" scoped>
+.dynamic-padding {
+  padding-left: calc(20 / 430 * 100%) !important;
+  padding-right: calc(20 / 430 * 100%) !important;
+  padding-bottom: calc(20 / 430 * 100%) !important;
+}
 </style>
