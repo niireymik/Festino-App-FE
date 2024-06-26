108e4d3be072126ecc4817ba5a05d86b9d65f858
heedong12 <dlgmldus3289@naver.com>
2024-06-27 02:52:10 +0900

    feat: make input to component

---
+++ src/components/tablings/InputName.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="text-xs">이름</div>
+  <div class="h-11 w-full flex flex-row border-b-1 border-secondary-500 items-center py-2.5 gap-2.5 mb-[30px]">
+    <img src="/icons/person.svg" class="w-6 h-6" />
+    <input
+      class="flex-1 focus:outline-none"
+      type="text"
+      :value="inputValue"
+      @input="(event) => updateInputValue(event.target.value)"
+      placeholder="티노"
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: '',
+  },
+});
+const emit = defineEmits(['update:modelValue']);
+
+const inputValue = ref(props.modelValue);
+
+const updateInputValue = (value) => {
+  inputValue.value = value;
+  emit('update:modelValue', value);
+};
+</script>
+
+<style lang="css" scoped></style>
