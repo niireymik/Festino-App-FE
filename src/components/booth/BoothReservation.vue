4e610cb478a857a76cdb7c49548ffc4a64ef0dde
김민지 <alswlk0306@gmail.com>
2024-06-27 21:38:35 +0900

    feat: booth reservation status component

---
+++ src/components/booth/BoothReservation.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="dynamic-padding">
+    <div class="font-pretendard text-2xl font-semibold">현재 대기중</div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="css" scoped>
+.dynamic-padding {
+  padding-left: calc(20 / 430 * 100%) !important;
+  padding-right: calc(20 / 430 * 100%) !important;
+}
+</style>
---
+++ src/views/BoothDetailView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <BoothMap />
+    <BoothReservation />
+  </div>
+</template>
+
+<script setup>
+import BoothMap from "@/components/booth/BoothMap.vue";
+import BoothReservation from "@/components/booth/BoothReservation.vue";
+</script>
+
+<style lang="css" scoped>
+</style>
