7af2e52ab127fd851ea0fb71ff6788edea7004ec
김민지 <alswlk0306@gmail.com>
2024-06-27 23:20:22 +0900

    feat: booth detail page

--- src/views/BoothDetailView.vue
+++ src/views/BoothDetailView.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <BoothMap />
+    <DetailImportmation />
+    <BoothInformation />
+    <DetailImportmation />
     <BoothReservation />
   </div>
 </template>
@@ -8,6 +11,8 @@
 <script setup>
 import BoothMap from "@/components/booth/BoothMap.vue";
 import BoothReservation from "@/components/booth/BoothReservation.vue";
+import DetailImportmation from "@/components/booth/DetailInformation.vue";
+import BoothInformation from "@/components/booth/BoothInformation.vue";
 </script>
 
 <style lang="css" scoped>
