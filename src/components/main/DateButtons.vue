3c00c74d3832fe8099f4566f1abd56fb073323cf
임정윤 <jeong.yun@tukorea.ac.kr>
2024-06-27 17:15:41 +0900

    refactor : componentize all buttons

---
+++ src/components/main/DateButtons.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex px-4 xs:px-5 pt-5 justify-between">
+    <div class="flex gap-2 px-[10px] xs:gap-4 xs:px-[12px] sm:gap-5 sm:px-[20px] py-[10px] rounded-full shadow-4xl text-xs items-center bg-primary-700 text-white font-bold">
+      <div>DAY 1</div>
+      <div>24.06.13</div>
+    </div>
+    <div class="flex gap-2 px-[10px] xs:gap-4 xs:px-[12px] sm:gap-5 sm:px-[20px] py-[10px] rounded-3xl shadow-4xl text-xs items-center text-primary-700-light font-normal border-primary border-1">
+      <div>DAY 2</div>
+      <div>24.06.14</div>
+    </div>
+    <div class="flex gap-2 px-[10px] xs:gap-4 xs:px-[12px] sm:gap-5 sm:px-[20px] py-[10px] rounded-3xl shadow-4xl text-xs items-center text-primary-700-light font-normal border-primary border-1">
+      <div>DAY 3</div>
+      <div>24.06.15</div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="css" scoped>
+</style>
