75d88c111710d626c53b05dd89691567b14c81a2
김민지 <alswlk0306@gmail.com>
2024-06-27 23:18:09 +0900

    feat: booth reservation

--- src/components/booth/BoothReservation.vue
+++ src/components/booth/BoothReservation.vue
@@ -1,6 +1,17 @@
 <template>
+  <div class="w-full h-3 mb-7 bg-secondary-200"></div>
   <div class="dynamic-padding">
-    <div class="font-pretendard text-2xl font-semibold">현재 대기중</div>
+    <div class="font-pretendard text-2xl font-semibold pb-5">현재 대기중</div>
+    <div class="mb-5 relative w-full min-h-[207px] bg-booth-reservation-status bg-no-repeat bg-cover">
+      <div class="absolute right-10 top-14 flex flex-col items-center">
+        <div class="px-4 py-1 mb-2 w-fit h-fit rounded-full bg-white text-primary-900 font-bold font-pretendard">대기중인 팀</div>
+        <div class="flex items-end">
+          <div class="font-bold text-7xl text-white font-pretendard">6</div>
+          <div class="pb-2 font-semibold text-xl text-white font-pretendard">팀</div>
+        </div>
+      </div>
+    </div>
+    <button class="w-full h-auto py-4 mt-3 mb-20 shadow-3xl border-1 border-primary-900 rounded-full text-primary-900 font-pretendard text-base active:text-white active:bg-primary-900">예약하기</button>
   </div>
 </template>
 
