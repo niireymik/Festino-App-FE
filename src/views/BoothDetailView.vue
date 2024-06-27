968b2684e528297e2a2da4ffee76401967048aa7
김민지 <alswlk0306@gmail.com>
2024-06-28 00:17:48 +0900

    feat: booth detail page banner

--- src/views/BoothDetailView.vue
+++ src/views/BoothDetailView.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <DetailBanner />
     <BoothMap />
     <DetailImportmation />
     <BoothInformation />
@@ -13,6 +14,7 @@ import BoothMap from "@/components/booth/BoothMap.vue";
 import BoothReservation from "@/components/booth/BoothReservation.vue";
 import DetailImportmation from "@/components/booth/DetailInformation.vue";
 import BoothInformation from "@/components/booth/BoothInformation.vue";
+import DetailBanner from "@/components/booth/DetailBanner.vue";
 </script>
 
 <style lang="css" scoped>
