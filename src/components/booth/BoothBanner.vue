fddbf03ab86d820dd432a829027b87cac3d58dce
김민지 <alswlk0306@gmail.com>
2024-06-27 21:11:24 +0900

    style: modify booth banner max height

--- src/components/booth/BoothBanner.vue
+++ src/components/booth/BoothBanner.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative">
-    <div class="w-full h-[260px] sm:h-[295px] bg-booth-banner bg-no-repeat bg-cover z-1">
+    <div class="w-full h-[260px] sm:max-h-[295px] bg-booth-banner bg-no-repeat bg-cover z-1">
       <i class="pi pi-arrow-left text-white text-xl absolute top-[20px] left-[20px] prev-btn-shadow"></i>
       <div class="absolute w-auto h-auto dynamic-top dynamic-padding">
         <div class="px-3 py-1 mb-2 w-fit flex justify-center text-center rounded-3.5xl border border-white text-white text-3xs font-pretendard">Festino</div>
