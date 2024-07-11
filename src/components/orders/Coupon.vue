2d0188f39f5550f7cdb3da52a26458c859002232
jzongyun <jeong.yun@tukorea.ac.kr>
2024-07-12 02:43:57 +0900

    feat : make coupon component

---
+++ src/components/orders/Coupon.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="p-4 w-full rounded-3xl border-2 border-primary gap-[14px] flex flex-col items-center justify-center">
+    <div class="flex w-full items-center gap-[14px]">
+      <div class="w-full">
+        <input class="w-full flex focus:outline-none py-[10px] placeholder:text-secondary-100" type="text" placeholder="쿠폰번호를 입력하세요." maxlength="10" />
+        <div class="w-full border-2 border-secondary-300 focus-within:border-primary-700"></div>
+      </div>
+      <div class="min-w-[54px] w-[54px] h-[30px] rounded-full bg-secondary-50 flex items-center justify-center text-secondary-300 text-sm">확인</div>
+    </div>
+    <div class="text-xs text-secondary-500">사이다 한 개 당첨 입니다.</div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+</style>
