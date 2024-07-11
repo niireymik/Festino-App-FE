c59a0bd1332b9b49be92f1d6b2f3b76a93ff0886
heedong12 <dlgmldus3289@naver.com>
2024-07-11 18:22:30 +0900

    feat: layout for main and order

--- src/router/index.js
+++ src/router/index.js
@@ -6,36 +6,50 @@ import TablingView from '@/views/TablingView.vue';
 import NotificationView from '@/views/NotificationView.vue';
 import BoothView from '@/views/BoothView.vue';
 import BoothDetailView from '@/views/BoothDetailView.vue';
+import OrderMainView from '@/views/orders/OrderMainView.vue';
+import MainLayout from '@/layouts/MainLayout.vue';
+import OrderLayout from '@/layouts/OrderLayout.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'main',
-      component: MainView,
+      component: MainLayout,
+      children: [
+        {
+          path: '',
+          name: 'main',
+          component: MainView,
+        },
+        {
+          path: 'timetable',
+          name: 'timetable',
+          component: TimeTableView,
+        },
+        { path: 'reserve', name: 'tabling', component: TablingView },
+        {
+          path: 'notification',
+          name: 'notification',
+          component: NotificationView,
+        },
+        {
+          path: 'booth',
+          name: 'booth',
+          component: BoothView,
+        },
+        {
+          path: 'booth/detail',
+          name: 'booth-detail',
+          component: BoothDetailView,
+        },
+      ],
     },
     {
-      path: '/timetable',
-      name: 'timetable',
-      component: TimeTableView,
+      path: '/order',
+      component: OrderLayout,
+      children: [{ path: '', name: 'order', component: OrderMainView }],
     },
-    { path: '/reserve', name: 'tabling', component: TablingView },
-    {
-      path: '/notification',
-      name: 'notification',
-      component: NotificationView,
-    },
-    {
-      path: '/booth',
-      name: 'booth',
-      component: BoothView,
-    },
-    {
-      path: '/booth/detail',
-      name: 'booth-detail',
-      component: BoothDetailView,
-    }
   ],
 });
 
