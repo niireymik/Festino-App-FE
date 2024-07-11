0b903eaae45691ebe19c4c0116eec2f26037cc9e
jzongyun <jeong.yun@tukorea.ac.kr>
2024-07-11 19:02:41 +0900

    style : change padding depend on design

--- src/components/notice/Notice.vue
+++ src/components/notice/Notice.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="w-full h-auto flex flex-row bg-white rounded-3xl border-primary border-1 justify-between items-center">
     <div class="flex flex-col pl-4 items-start py-3">
-      <div class="text-primary-700 text-2xs px-[12px] py-[2px] rounded-xl font-medium border-primary border-1">Festino</div>
       <div class="py-1 text-base font-normal text-secondary-500 leading-tight">'Festino' 사용 공지사항</div>
     </div>
-    <div class="flex justify-end pr-[11px] text-xs font-normal text-secondary-500 w-[140px]">1시간 전</div>
+    <div class="flex justify-end pr-4 text-xs font-normal text-secondary-500 w-[140px]">1시간 전</div>
   </div>
 </template>
 
