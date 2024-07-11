74730bc51db55e0af0f6066502088e8a3a6f02b7
heedong12 <dlgmldus3289@naver.com>
2024-07-12 02:44:01 +0900

    feat: move to main when click back button of header

--- src/components/orders/Header.vue
+++ src/components/orders/Header.vue
@@ -1,14 +1,19 @@
 <script setup>
+import router from '@/router';
+
 const props = defineProps({
   title: {
     type: String,
     default: '',
   },
 });
+const handleClickBackButton = () => {
+  router.push({ name: 'order' });
+};
 </script>
 <template>
   <div class="w-full h-[60px] bg-white flex justify-between items-center px-6 shadow-xs mb-7">
-    <img src="/icons/header-arrow-back.svg" class="w-7" />
+    <img src="/icons/header-arrow-back.svg" class="w-7" @click="handleClickBackButton()" />
     <p class="font-medium text-xl">{{ title }}</p>
     <div class="w-7"></div>
   </div>
