23f1fa66e31a5f944a836f21867d1b95a4ab31e0
jzongyun <jeong.yun@tukorea.ac.kr>
2024-07-12 02:43:41 +0900

    refactor : make header present on the page

--- src/components/orders/Header.vue
+++ src/components/orders/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full h-[60px] bg-white flex justify-between items-center px-6 shadow-xs">
+  <div class="fixed top-0 max-w-[500px] w-full h-[60px] bg-white flex justify-between items-center px-6 shadow-xs">
     <img src="/icons/header-arrow-back.svg" class="w-7" />
     <p class="font-medium text-xl">주문 하기</p>
     <div class="w-7"></div>
