7997dad8909523534009e8900186c58659f07f3a
heedong12 <dlgmldus3289@naver.com>
2024-07-14 02:30:30 +0900

    style: modify not to hide word

--- src/components/orders/OrderMainBanner.vue
+++ src/components/orders/OrderMainBanner.vue
@@ -1,8 +1,8 @@
 <template>
   <div
-    class="w-full h-full min-h-[290px] xs:min-h-[280px] sm:min-h-[270px] bg-tino-order-banner bg-no-repeat bg-cover bg-top relative"
+    class="w-full sm:h-[290px] xs:h-[280px] max-xs:h-[270px] bg-tino-order-banner bg-no-repeat bg-cover bg-top relative"
   >
-    <div class="absolute top-14 left-5 w-auto h-auto font-jalnan2">
+    <div class="absolute max-xs:top-10 top-14 left-5 w-auto h-auto font-jalnan2">
       <p class="text-white">내 자리에서 주문부터 결제까지!</p>
       <p class="text-3xl bg-gradient-to-t from-white-opacity from-20% to-white text-transparent to-100% bg-clip-text">
         간편 주문 시스템
