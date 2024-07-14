14000d440e0e70fda12beac21c2accc7fb468b9c
heedong12 <dlgmldus3289@naver.com>
2024-07-14 20:14:11 +0900

    design: show left item

--- src/components/tablings/Reservation.vue
+++ src/components/tablings/Reservation.vue
@@ -20,8 +20,9 @@ const handleClickReserveButton = () => {
 <template>
   <div class="w-screen max-w-[500px] min-w-[375px]">
     <div class="w-full flex justify-center">
-      <div class="dynamic-grid-container overflow-x-auto pt-10" @touchstart.stop="" id="reserve-container">
+      <div class="overflow-x-auto pt-10" @touchstart.stop="" id="reserve-container">
         <div class="grid w-auto grid-rows-2 gap-2 grid-flow-col">
+          <div class="row-span-2 dynamic-width"></div>
           <div
             v-for="(item, index) in 20"
             class="bg-primary-300 aspect-w-1 aspect-h-1 dynamic-item rounded-3xl"
@@ -36,6 +37,7 @@ const handleClickReserveButton = () => {
               <h2 class="text-2xs">대기중인 팀 : 9</h2>
             </div>
           </div>
+          <div class="row-span-2 dynamic-width"></div>
         </div>
       </div>
     </div>
@@ -69,10 +71,6 @@ const handleClickReserveButton = () => {
   min-width: calc(375px * 190 / 430) !important;
 }
 
-.dynamic-grid-container {
-  width: calc(390 / 430 * 100%) !important;
-}
-
 .dynamic-margin {
   margin-left: calc(20 / 430 * 100%) !important;
   margin-right: calc(20 / 430 * 100%) !important;
@@ -81,6 +79,10 @@ const handleClickReserveButton = () => {
 .dynamic-padding {
   padding-left: calc(20 / 430 * 100%) !important;
   padding-right: calc(20 / 430 * 100%) !important;
+}
+
+.dynamic-width {
+  width: calc(100vw * 10 / 430) !important;
 }
 
 #reserve-container::-webkit-scrollbar {
