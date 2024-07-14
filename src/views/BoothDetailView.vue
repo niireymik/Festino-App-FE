4984b9f6a3effdb03bd890267252882feb090c16
김민지 <alswlk0306@gmail.com>
2024-07-14 23:59:25 +0900

    feat: separate pages by booth

--- src/views/BoothDetailView.vue
+++ src/views/BoothDetailView.vue
@@ -1,15 +1,3 @@
-<template>
-  <div>
-    <DetailBanner />
-    <BoothSelect />
-    <BoothMap />
-    <DetailImportmation />
-    <BoothInformation />
-    <MenuList />
-    <BoothReservation />
-  </div>
-</template>
-
 <script setup>
 import BoothMap from "@/components/booth/BoothMap.vue";
 import BoothReservation from "@/components/booth/BoothReservation.vue";
@@ -18,7 +6,23 @@ import BoothInformation from "@/components/booth/BoothInformation.vue";
 import DetailBanner from "@/components/booth/DetailBanner.vue";
 import BoothSelect from "@/components/booth/BoothSelect.vue";
 import MenuList from "@/components/booth/MenuList.vue";
+import { useGetBoothDataStore } from "@/stores/booths/boothDataStore";
+import { storeToRefs } from "pinia";
+
+const { boothType } = storeToRefs(useGetBoothDataStore());
 </script>
+
+<template>
+  <div class="mb-12">
+    <DetailBanner />
+    <BoothSelect />
+    <BoothMap />
+    <DetailImportmation />
+    <BoothInformation />
+    <MenuList v-if="boothType === '운동장'" />
+    <BoothReservation v-if="boothType === '운동장'" />
+  </div>
+</template>
 
 <style lang="css" scoped>
 </style>
