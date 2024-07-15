b0389cfcf824171736ab3ac63327086a56234a21
김민지 <alswlk0306@gmail.com>
2024-07-16 00:34:46 +0900

    style: rename item to booth

--- src/components/booth/BoothIntroduction.vue
+++ src/components/booth/BoothIntroduction.vue
@@ -26,7 +26,7 @@ const handleClickBoothIntroduction = (type, id) => {
 
 <template>
   <div class="dynamic-padding w-full h-auto">
-    <div @click="handleClickBoothIntroduction(item.adminCategory, item.boothId)" v-for="(item, index) in boothList[selectBoothMenu]" :key="index" class="pb-2 cursor-pointer">
+    <div @click="handleClickBoothIntroduction(booth.adminCategory, booth.boothId)" v-for="(booth, index) in boothList[selectBoothMenu]" :key="index" class="pb-2 cursor-pointer">
       <div
         class="w-full h-[160px] bg-white shadow-3xl flex flex-row justify-between items-center rounded-3.5xl border border-primary-900-light-16 px-4 py-3"
       >
@@ -34,19 +34,19 @@ const handleClickBoothIntroduction = (type, id) => {
           <div
             class="px-2 py-1 w-fit flex justify-center text-center rounded-3.5xl border border-primary-900 text-primary-900 text-3xs font-pretendard font-semibold"
           >
-            #{{ item.adminCategory }}
+            #{{ booth.adminCategory }}
           </div>
-          <div class="py-1.5 text-base font-pretendard font-semibold">{{ item.boothName }}</div>
+          <div class="py-1.5 text-base font-pretendard font-semibold">{{ booth.boothName }}</div>
           <div class="pb-2 text-2xs text-secondary-500">
-            {{ item.boothIntro }}
+            {{ booth.boothIntro }}
           </div>
           <div class="flex flex-row">
-            <ShowState :isState="item.isOpen">{{ item.isOpen ? '운영중' : '준비중' }}</ShowState>
+            <ShowState :isState="booth.isOpen">{{ booth.isOpen ? '운영중' : '준비중' }}</ShowState>
             <div
               class="px-2 py-1 w-fit flex justify-center text-center items-center text-3xs text-secondary-500 bg-primary-100 rounded-full"
             >
               <img class="mr-1 w-2 h-2 flex justify-center" src="/images/booth/booth-clock-icon.png" />
-              <div>{{ item.openTime }} ~ {{ item.closeTime }}</div>
+              <div>{{ booth.openTime }} ~ {{ booth.closeTime }}</div>
             </div>
           </div>
         </div>
