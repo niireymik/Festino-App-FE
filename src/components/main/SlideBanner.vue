f84b92ac1128d9fcb844ff3f3a845dbeab480f8c
김민지 <alswlk0306@gmail.com>
2024-07-19 00:20:09 +0900

    fix: router error

--- src/components/main/SlideBanner.vue
+++ src/components/main/SlideBanner.vue
@@ -54,7 +54,7 @@ const handleClickMoveBooth = () => {
 };
 
 const handleClickMoveTabling = () => {
-  router.push({ name: 'tabling' });
+  router.push({ name: 'reserve' });
 };
 
 const handleClickMoveTimeTable = () => {
