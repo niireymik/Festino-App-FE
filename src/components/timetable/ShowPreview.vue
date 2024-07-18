f7f45b946a6364a9bc98236da0a0b6fc26b7108c
jzongyun <jeong.yun@tukorea.ac.kr>
2024-07-18 21:06:11 +0900

    feat : when show increase scroll on

--- src/components/timetable/ShowPreview.vue
+++ src/components/timetable/ShowPreview.vue
@@ -40,7 +40,7 @@ onMounted(async () => {
 <template>
   <div class="px-5">
     <div class="w-full h-[178px] bg-white rounded-3xl border-primary border-1 flex justify-center select-none" v-bind="$attrs">
-      <div class="flex pt-[17px] w-full dynamic-padding justify-evenly">
+      <div class="flex pt-[17px] w-full px-3 justify-evenly gap-1 overflow-x-auto reserve-container">
         <div v-if="!showData.length" class="pt-14">공연정보가 없습니다.</div>
         <div class="flex flex-col items-center cursor-pointer" v-for="show in showData" :key="show.id" @click="handleClickOpenModal(props.category, show)">
           <div class="w-[100px] h-[100px] bg-primary-700 rounded-full bg-tino-cheer-up bg-center bg-no-repeat bg-[length:61px_62px]"></div>
@@ -52,9 +52,13 @@ onMounted(async () => {
   </div>
 </template>
 
-<style lang="css" scoped>
-.dynamic-padding {
-  padding-left: calc(16 / 390 * 100%) !important;
-  padding-right: calc(16 / 390 * 100%) !important;
+<style scoped>
+.reserve-container {
+  scrollbar-width: none;
+  -ms-overflow-style: none;
+}
+
+.reserve-container::-webkit-scrollbar {
+  display: none;
 }
 </style>
