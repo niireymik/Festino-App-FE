a21536ff30c9bcd5aa648412ae3df0fe36f9d704
heedong12 <dlgmldus3289@naver.com>
2024-07-18 19:48:43 +0900

    fix: do not use inline style

--- src/components/tablings/Reservation.vue
+++ src/components/tablings/Reservation.vue
@@ -39,6 +39,14 @@ const nightBoothInfoLength = ref(0);
 watch(nightBoothInfo, () => {
   nightBoothInfoLength.value = nightBoothInfo.value.length;
 });
+
+const getNightBoothImage = (nightBoothImage) => {
+  return {
+    style: nightBoothImage
+      ? `background-image: url(${nightBoothImage})`
+      : 'background-image: url(/images/booth/booth-default-image.png)',
+  };
+};
 </script>
 <template>
   <div class="w-screen max-w-[500px] min-w-[375px]">
@@ -56,7 +64,7 @@ watch(nightBoothInfo, () => {
             :key="nightBooth.boothId"
             @click="handleClickMajorBox(nightBooth)"
             class="aspect-w-1 aspect-h-1 dynamic-item rounded-3xl bg-no-repeat bg-cover"
-            :style="{ backgroundImage: `url(${nightBooth.boothImage})` }"
+            v-bind="getNightBoothImage(nightBooth.boothImage)"
           >
             <div
               class="flex flex-col justify-end text-white p-5 bg-gradient-to-t from-black to-white rounded-3xl opacity-40"
