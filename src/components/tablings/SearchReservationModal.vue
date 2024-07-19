b2dd36b7f27627a087770fe6d33234b712e60ee1
heedong12 <dlgmldus3289@naver.com>
2024-07-20 01:23:50 +0900

    feat: use api data

--- src/components/tablings/SearchReservationModal.vue
+++ src/components/tablings/SearchReservationModal.vue
@@ -45,7 +45,7 @@ onMounted(() => {
       <div class="flex flex-col w-full h-[78px] bg-primary-900-light-6 rounded-lg-xl gap-3 p-4 text-sm justify-center">
         <div class="flex flex-row justify-between">
           <div>대기번호</div>
-          <div>103번</div>
+          <div>{{ reservationInfo.reservationNum }}번</div>
         </div>
         <div class="flex flex-row justify-between">
           <div>인원</div>
@@ -56,14 +56,12 @@ onMounted(() => {
         <span class="text-base">※ </span>
         대기팀이 1팀일 때 알림이 전송됩니다.
       </div>
-      <!-- <div class="w-full flex flex-row justify-between gap-[10px]"> -->
       <button
         class="w-full h-[43px] text-white font-bold rounded-10xl border-1 border-primary-900-light-68 shrink-0 bg-primary-900"
         @click="closeSearchReserveModal()"
       >
         확인
       </button>
-      <!-- </div> -->
     </div>
   </div>
 </template>
