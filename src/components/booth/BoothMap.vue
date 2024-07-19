08fe7b5c961d350b75d75db498fa5ab9d94d74c4
김민지 <alswlk0306@gmail.com>
2024-07-19 17:51:03 +0900

    feat: booth map

--- src/components/booth/BoothMap.vue
+++ src/components/booth/BoothMap.vue
@@ -1,15 +1,36 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+
+const zoomLevel = ref(1);
+
+const zoomIn = () => {
+  zoomLevel.value = Math.min(zoomLevel.value + 0.2, 2);
+};
+
+const zoomOut = () => {
+  zoomLevel.value = Math.max(zoomLevel.value - 0.2, 1);
+};
+</script>
 
 <template>
-  <div class="dynamic-padding w-full h-auto">
-    <div class="relative w-full min-h-[340px] sm:h-[440px] bg-primary-300 border border-primary-900-light rounded-3xl">
+  <div class="dynamic-padding">
+    <div class="relative">
+      <div class="w-full min-h-[340px] sm:h-[440px] border border-primary-900-light rounded-3xl overflow-auto touch-manipulation">
+        <img
+          class="max-w-none h-auto"
+          :style="{ width: `${zoomLevel * 160}%`, transform: `scale(${zoomLevel})`, transformOrigin: 'left top' }"
+          src="/images/booth/map.svg"
+        />
+      </div>
       <div class="absolute bottom-5 left-5">
         <button
+          @click="zoomIn"
           class="shadow-3xl bg-white rounded-t-full border-1 border-primary-900-light p-4 flex justify-center items-center active:bg-primary-900 active:text-white"
         >
           <i class="pi pi-plus text-primary-900 text-lg active:text-white"></i>
         </button>
         <button
+          @click="zoomOut"
           class="shadow-3xl bg-white rounded-b-full border-1 border-primary-900-light p-4 flex justify-center items-center active:bg-primary-900 active:text-white"
         >
           <i class="pi pi-minus text-primary-900 text-lg active:text-white"></i>
@@ -19,7 +40,7 @@
   </div>
 </template>
 
-<style lang="css" scoped>
+<style lang="css">
 .dynamic-padding {
   padding-top: calc(10 / 430 * 100%) !important;
   padding-left: calc(20 / 430 * 100%) !important;
