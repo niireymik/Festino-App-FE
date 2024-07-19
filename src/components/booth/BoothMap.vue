d2390d496188a10ae599ec5c60289ba6e6409797
김민지 <alswlk0306@gmail.com>
2024-07-20 02:18:14 +0900

    fix: night booth marking error

--- src/components/booth/BoothMap.vue
+++ src/components/booth/BoothMap.vue
@@ -7,20 +7,20 @@ const { selectBoothMenu } = storeToRefs(useGetBoothDataStore());
 
 const zoomLevel = ref(1);
 const containerRef = ref(null);
-const imageLoaded = ref(-1);
+const imageLoaded = ref(false);
 const markers = ref([
-  { left: 140, bottom: 152 },
-  { left: 185, bottom: 152 },
-  { left: 230, bottom: 152 },
-  { left: 275, bottom: 152 },
-  { left: 320, bottom: 152 },
-  { left: 100, bottom: 185 },
-  { left: 100, bottom: 235 },
-  { left: 100, bottom: 285 },
-  { left: 140, bottom: 330 },
-  { left: 185, bottom: 330 },
-  { left: 230, bottom: 330 },
-  { left: 275, bottom: 330 },
+  { left: 120, bottom: 135 },
+  { left: 160, bottom: 135 },
+  { left: 200, bottom: 135 },
+  { left: 240, bottom: 135 },
+  { left: 280, bottom: 135 },
+  { left: 80, bottom: 160 },
+  { left: 80, bottom: 205 },
+  { left: 80, bottom: 250 },
+  { left: 120, bottom: 280 },
+  { left: 160, bottom: 280 },
+  { left: 200, bottom: 280 },
+  { left: 240, bottom: 280 },
 ]);
 
 const selectedMarker = ref(null);
@@ -80,9 +80,10 @@ watch([zoomLevel, imageLoaded, selectBoothMenu], () => {
           :style="{ width: `${zoomLevel * 250}%` }"
         >
           <img
+            ref="imageRef"
             src="/images/booth/map.svg"
             alt="Booth Map"
-            class="w-full"
+            class="w-[1000px] h-auto max-w-full"
             @load="imageLoaded = true"
           />
           <div 
@@ -90,12 +91,12 @@ watch([zoomLevel, imageLoaded, selectBoothMenu], () => {
             :key="index"
             class="absolute marker"
             :style="{
-              left: `${marker.left * zoomLevel}px`,
-              bottom: `${marker.bottom * zoomLevel}px`,
+              left: `calc(${marker.left}px`,
+              bottom: `calc(${marker.bottom}px`,
               transform: `scale(${selectedMarker === index ? 1.25 : 1}) translateY(${selectedMarker === index ? (1.25 - 1) : 0}px)`,
               opacity: selectedMarker === index ? '1' : '0.75',
-              width: `${selectedMarker === index ? 51 : 45 * zoomLevel}px`,
-              height: `${selectedMarker === index ? 50 : 44 * zoomLevel}px`
+              width: `${selectedMarker === index ? 51 / zoomLevel : 45 / zoomLevel}px`,
+              height: `${selectedMarker === index ? 50 / zoomLevel : 44 / zoomLevel}px`
             }"
             @click="handleMarkerClick(index)"
           >
