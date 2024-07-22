46b51c6bfbd8aa8677d0bd8341dd0dd1a54b0224
김민지 <alswlk0306@gmail.com>
2024-07-23 03:14:59 +0900

    fix: image drag none, and menu information layout

--- src/components/booth/BoothReservation.vue
+++ src/components/booth/BoothReservation.vue
@@ -15,7 +15,7 @@ const handleRoutertoReserve = () => {
   <div class="dynamic-padding">
     <div class="font-pretendard text-2xl font-semibold pb-5">현재 대기중</div>
     <div class="mb-5 relative aspect-auto w-full h-[155px] xs:h-[171.3px] sm:h-[207px] bg-booth-reservation-status bg-no-repeat bg-cover rounded-3xl">
-      <div class="absolute right-10 top-6 xs:top-8 sm:top-14 flex flex-col items-center">
+      <div class="absolute right-8 xs:right-10 top-6 xs:top-8 sm:top-12 flex flex-col items-center">
         <div class="px-4 py-1 mb-2 w-fit h-fit rounded-full bg-white text-primary-900 font-bold font-pretendard">대기중인 팀</div>
         <div class="flex items-end">
           <div class="font-bold text-7xl text-white font-pretendard">{{ booth.totalReservationNum }}</div>
