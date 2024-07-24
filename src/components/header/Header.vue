598ef0b70d06c55ad89d119f20f8fadddb9018d3
jzongyun <jeong.yun@tukorea.ac.kr>
2024-07-25 01:23:24 +0900

    fix : fix image error

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full h-[58px] flex flex-row bg-white justify-between px-5 items-center">
     <div class="w-[68px] h-[36px] bg-header-tino-logo bg-center bg-no-repeat bg-[length:68px_36px]" @click="handleClickMainSymbol()"></div>
-    <div class="w-[32px] h-[32px] bg-header-team-introduction bg-center bg-no-repeat bg-[length:32px_32px]"></div>
+    <div class="w-[32px] h-[32px] bg-header-team-introduction bg-center bg-no-repeat bg-[length:32px_32px]" @click="moveToDeveloperPage()"></div>
   </div>
 </template>
 
@@ -12,6 +12,10 @@ const router = useRouter();
 
 const handleClickMainSymbol = () => {
   router.push({ name: "main"});
+};
+
+const moveToDeveloperPage = () => {
+  window.open(`https://github.com/DEV-TINO`, "_blank");
 };
 </script>
 
