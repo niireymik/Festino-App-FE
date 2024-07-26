8b6ad047373f68c09ca65daa3931ccfe694b2eee
김민지 <alswlk0306@gmail.com>
2024-07-27 04:28:32 +0900

    feat: map speech bubble data binding

--- src/components/booth/MapSpeechBubble.vue
+++ src/components/booth/MapSpeechBubble.vue
@@ -1,24 +1,28 @@
 <script setup>
+import { useGetBoothDataStore } from '@/stores/booths/boothDataStore';
+import { storeToRefs } from 'pinia';
+
+const { boothMarkerData } = storeToRefs(useGetBoothDataStore());
 </script>
 
 <template>
-  <div class="px-[18px] py-[11px] speech-bubble shadow-5xl flex flex-col">
-    <div class="text-primary-900 font-semibold text-[12px] pb-1">디자인공학부 부스</div>
+  <div class="px-[18px] py-[11px] speech-bubble shadow-5xl flex flex-col justify-center">
+    <div class="text-primary-900 font-semibold text-[11px] pb-1">{{ boothMarkerData?.adminCategory !== '편의시설' ? `${boothMarkerData.adminName} 부스` : boothMarkerData.boothName }}</div>
 		<div class="flex items-center">
 			<div class="w-[10px] h-[10px] bg-[url('/icons/booth/location_on.svg')]"></div>
-			<div class="text-[8px] pl-[2px] text-secondary-500">운동장 구령대 앞</div>
+			<div class="text-[8px] pl-[2px] text-secondary-500">{{ boothMarkerData.location }}</div>
 		</div>
 		<div class="flex items-center">
 			<div class="w-[10px] h-[10px] bg-[url('/icons/booth/alarm.svg')]"></div>
-			<div class="text-[8px] pl-[2px] text-secondary-500">18:00 ~ 24:00</div>
+			<div class="text-[8px] pl-[2px] text-secondary-500">{{ boothMarkerData.openTime }} ~ {{ boothMarkerData.closeTime }}</div>
 		</div>
   </div>
 </template>
 
 <style lang="css" scoped>
 .speech-bubble {
-  width: 132px;
-  height: 68px;
+  min-width: 150px;
+  min-height: 75px;
 	position: relative;
 	background-color: #ffffff;
 	border-radius: 17px;
@@ -31,10 +35,10 @@
 	left: 50%;
 	width: 0;
 	height: 0;
-	border: 18px solid transparent;
+	border: 10px solid transparent;
 	border-top-color: #ffffff;
 	border-bottom: 0;
-	margin-left: -18px;
-	margin-bottom: -16px;
+	margin-left: -10px;
+	margin-bottom: -9px;
 }
 </style>
