d5d0231a64ef7f61d7ac8767a82bce432c8919e9
김민지 <alswlk0306@gmail.com>
2024-07-29 06:55:23 +0900

    style: header mouse pointer

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full h-[58px] flex flex-row bg-white justify-between px-5 items-center">
-    <div class="w-[68px] h-[36px] bg-header-tino-logo bg-center bg-no-repeat bg-[length:68px_36px]" @click="handleClickMainSymbol()"></div>
-    <div class="w-[32px] h-[32px] bg-header-team-introduction bg-center bg-no-repeat bg-[length:32px_32px]" @click="moveToDeveloperPage()"></div>
+    <div class="w-[68px] h-[36px] bg-header-tino-logo bg-center bg-no-repeat bg-[length:68px_36px] cursor-pointer" @click="handleClickMainSymbol()"></div>
+    <div class="w-[32px] h-[32px] bg-header-team-introduction bg-center bg-no-repeat bg-[length:32px_32px] cursor-pointer" @click="moveToDeveloperPage()"></div>
   </div>
 </template>
 
