f4c64d0a10b5e9a89fbf6a8b6fc282ed20e29c83
jzongyun <jeong.yun@tukorea.ac.kr>
2024-07-28 23:51:48 +0900

    fix : remove duplicate word

--- src/utils/formatPrice.js
+++ src/utils/formatPrice.js
@@ -1,5 +1,5 @@
 export const formatPrice = (price) => {
   if (price === '') return '';
   if (typeof price === 'string') price = parseInt(price);
-  return price.toLocaleString('ko-KR') + '원';
+  return price.toLocaleString('ko-KR');
 };
