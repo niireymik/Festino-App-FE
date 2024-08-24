abfc84c4d6cc7c5e9fa74eaa2d53c959f245716b
김민지 <alswlk0306@gmail.com>
2024-08-25 00:46:10 +0900

    feat: add day booth menu list

--- src/components/booth/MenuList.vue
+++ src/components/booth/MenuList.vue
@@ -1,12 +1,16 @@
 <script setup>
 import MenuInformation from "./MenuInformation.vue";
 import MenuSummary from "@/components/booth/MenuSummary.vue";
+import { useGetBoothDataStore } from "@/stores/booths/boothDataStore";
+import { storeToRefs } from "pinia";
+
+const { booth } = storeToRefs(useGetBoothDataStore());
 </script>
 
 <template>
   <div class="w-full h-3 mb-7 bg-secondary-200"></div>
   <div class="mb-[28px]">
-    <MenuSummary />
+    <MenuSummary v-if="booth.adminCategory === 'night'"  />
     <MenuInformation />
   </div>
 </template>
