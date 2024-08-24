f8428f1c988dd6b8b20c8f9e2e3126ad907491ec
kim-song-jun <>
2024-08-24 19:41:49 +0900

    feat : seo

--- src/App.vue
+++ src/App.vue
@@ -57,18 +57,18 @@ onMounted(() => {
   resetModalState();
 });
 
-useHead({
-  title: 'Festino!',
-  meta: [
-    { name: 'description', content: 'Festino! 모두함께 즐기는 한국공학교 대동제!', vmid: 'description' },
-    { name: 'keywords', content: 'Festino, 한국공학대학교, 한국공대, 축제', vmid: 'keywords' },
-    { property: 'og:title', content: 'Festino!', vmid: 'og:title' },
-    { property: 'og:description', content: 'Festino! 모두함께 즐기는 한국공학교 대동제!', vmid: 'og:description' },
-    { property: 'og:image', content: '/images/seo/title-images.png', vmid: 'og:image' },
-    { property: 'og:url', content: 'https://festino.dev-tino.com', vmid: 'og:url' },
-    { property: 'og:type', content: 'website', vmid: 'og:type' },
-  ],
-});
+// useHead({
+//   title: 'Festino!',
+//   meta: [
+//     { name: 'description', content: 'Festino! 모두함께 즐기는 한국공학교 대동제!', vmid: 'description' },
+//     { name: 'keywords', content: 'Festino, 한국공학대학교, 한국공대, 축제', vmid: 'keywords' },
+//     { property: 'og:title', content: 'Festino!', vmid: 'og:title' },
+//     { property: 'og:description', content: 'Festino! 모두함께 즐기는 한국공학교 대동제!', vmid: 'og:description' },
+//     { property: 'og:image', content: 'https://festino.dev-tino.com/images/seo/title-images.png', vmid: 'og:image' },
+//     { property: 'og:url', content: 'https://festino.dev-tino.com', vmid: 'og:url' },
+//     { property: 'og:type', content: 'website', vmid: 'og:type' },
+//   ],
+// });
 </script>
 
 <template>
