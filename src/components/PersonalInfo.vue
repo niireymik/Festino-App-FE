3ebbcf49bd2175d5d1c13fdc8914c913d51d46f4
heedong12 <dlgmldus3289@naver.com>
2024-08-26 03:02:46 +0900

    fix: checkbox error

--- src/components/PersonalInfo.vue
+++ src/components/PersonalInfo.vue
@@ -5,8 +5,13 @@ const { handleClickAgreeCheckBox } = usePersonalInfoStore();
 </script>
 
 <template>
-  <label for="agree-checkbox" class="flex items-center text-sm font-medium text-secondary-700">
-    <input @click.stop="handleClickAgreeCheckBox()" id="agree-checkbox" type="checkbox" value="" class="w-4 h-4 mr-2 text-primary-900 bg-gray-100 border-gray-300 rounded-4xl focus:ring-primary-900 focus:ring-offset-1 focus:ring-1 focus:rounded-3xl">
+  <label class="flex items-center text-sm font-medium text-secondary-700">
+    <input
+      @click.stop="handleClickAgreeCheckBox()"
+      type="checkbox"
+      value=""
+      class="w-4 h-4 mr-2 text-primary-900 bg-gray-100 border-gray-300 rounded-4xl focus:ring-primary-900 focus:ring-offset-1 focus:ring-1 focus:rounded-3xl"
+    />
     개인정보 수집• 이용 동의 (필수)
   </label>
 </template>
