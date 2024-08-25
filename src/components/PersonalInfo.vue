87e8dd70c3024e00dd7e34f6537acc58002b1e8d
heedong12 <dlgmldus3289@naver.com>
2024-08-26 03:04:51 +0900

    feat: add space

--- src/components/PersonalInfo.vue
+++ src/components/PersonalInfo.vue
@@ -12,7 +12,7 @@ const { handleClickAgreeCheckBox } = usePersonalInfoStore();
       value=""
       class="w-4 h-4 mr-2 text-primary-900 bg-gray-100 border-gray-300 rounded-4xl focus:ring-primary-900 focus:ring-offset-1 focus:ring-1 focus:rounded-3xl"
     />
-    개인정보 수집• 이용 동의 (필수)
+    개인정보 수집 • 이용 동의 (필수)
   </label>
 </template>
 
