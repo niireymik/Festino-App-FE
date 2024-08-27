7d898f9f3cf30df62e9b4ea1b1767a7f8b58b1d2
김민지 <alswlk0306@gmail.com>
2024-08-27 18:47:38 +0900

    fix: tabling page personal information checkbox error

--- src/components/PersonalInfo.vue
+++ src/components/PersonalInfo.vue
@@ -1,13 +1,16 @@
 <script setup>
 import { usePersonalInfoStore } from '@/stores/personalInfoStore';
+import { storeToRefs } from 'pinia';
 
+const { isAgreed } = storeToRefs(usePersonalInfoStore());
 const { handleClickAgreeCheckBox } = usePersonalInfoStore();
 </script>
 
 <template>
   <label class="flex items-center text-sm font-medium text-secondary-700">
     <input
-      @click.stop="handleClickAgreeCheckBox()"
+      v-model="isAgreed"
+      @click.stop="handleClickAgreeCheckBox"
       type="checkbox"
       value=""
       class="w-4 h-4 mr-2 text-primary-900 bg-gray-100 border-gray-300 rounded-4xl focus:ring-primary-900 focus:ring-offset-1 focus:ring-1 focus:rounded-3xl"
