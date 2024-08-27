188224291a0268459001e49b9b05b238b901652b
김민지 <alswlk0306@gmail.com>
2024-08-27 14:16:54 +0900

    fix: keep your booth tabbed

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -2,12 +2,17 @@
 import MainBanner from '@/components/main/MainBanner.vue';
 import Header from '@/components/header/Header.vue';
 import MainTab from '@/components/main/MainTab.vue';
+import { useGetBoothDataStore } from '@/stores/booths/boothDataStore';
 
 import { onMounted } from 'vue';
+import { storeToRefs } from 'pinia';
+
+const { selectBoothMenu } = storeToRefs(useGetBoothDataStore());
 
 onMounted(() => {
   document.documentElement.scrollTop = 0;
   document.body.scrollTop = 0;
+  selectBoothMenu.value = 0;
 });
 </script>
 
