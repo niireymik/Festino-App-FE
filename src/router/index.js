9b3706beb93e71dbcf1f9c7559b5654367d129f7
heedong12 <dlgmldus3289@naver.com>
2024-08-28 18:22:42 +0900

    feat: close modal when page move

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,7 @@ import OrderLayout from '@/layouts/OrderLayout.vue';
 import OrderPaymentView from '@/views/orders/OrderPaymentView.vue';
 import OrderSearch from '@/views/orders/OrderSearch.vue';
 import ErrorView from '@/views/ErrorView.vue';
+import { useBaseModal } from '@/stores/baseModal';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -88,6 +89,14 @@ const router = createRouter({
       component: ErrorView,
     },
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  const baseModalStore = useBaseModal();
+
+  console.log('이동함');
+  baseModalStore.closeModal();
+  next();
 });
 
 trackRouter(router);
