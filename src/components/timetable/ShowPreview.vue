b6bd5ad358329230c310f735f7e8622114aa2a80
kim-song-jun <>
2024-09-05 01:29:17 +0900

    fix : fix default image

--- src/components/timetable/ShowPreview.vue
+++ src/components/timetable/ShowPreview.vue
@@ -9,7 +9,7 @@ const { getClubTimetable, getTalentTimetable } = useTimetableStore();
 const { clubData, talentData, day } = storeToRefs(useTimetableStore());
 const showData = ref([]);
 
-const props = defineProps({   
+const props = defineProps({
   category: {
     type: String,
     required: true,
@@ -17,22 +17,26 @@ const props = defineProps({
 });
 
 const getImage = (data) => {
-  if(data.talentImage) return { backgroundImage: `url('${data.talentImage}')` }
-  if(data.clubImage) return { backgroundImage: `url('${data.clubImage}')` }
+  if (data.talentImage) return { backgroundImage: `url('${data.talentImage}')` };
+  if (data.clubImage) return { backgroundImage: `url('${data.clubImage}')` };
+  return { backgroundImage: `url('/images/booth/booth-default-image.png')` };
 };
 
-watch(() => day.value, async () => {
-  if (props.category === "talent") {
-    await getTalentTimetable();
-    showData.value = talentData.value.showInfo;
-  } else {
-    await getClubTimetable();
-    showData.value = clubData.value.showInfo;
-  }
-});
+watch(
+  () => day.value,
+  async () => {
+    if (props.category === 'talent') {
+      await getTalentTimetable();
+      showData.value = talentData.value.showInfo;
+    } else {
+      await getClubTimetable();
+      showData.value = clubData.value.showInfo;
+    }
+  },
+);
 
 onMounted(async () => {
-  if (props.category === "talent") {
+  if (props.category === 'talent') {
     await getTalentTimetable();
     showData.value = talentData.value.showInfo;
   } else {
@@ -43,16 +47,32 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div v-if="showData" class="w-full h-[178px] bg-white rounded-3xl border-primary border-1 flex justify-center select-none" v-bind="$attrs">
+  <div
+    v-if="showData"
+    class="w-full h-[178px] bg-white rounded-3xl border-primary border-1 flex justify-center select-none"
+    v-bind="$attrs"
+  >
     <div class="flex pt-[17px] w-full px-3 justify-evenly gap-1 overflow-x-auto reserve-container">
-      <div class="flex flex-col items-center cursor-pointer" v-for="show in showData" :key="show" @click="handleClickOpenModal(props.category, show)">
-        <div :style="getImage(show)" class="border-2 border-primary bg-cover bg-center w-[100px] h-[100px] rounded-full"></div>
+      <div
+        class="flex flex-col items-center cursor-pointer"
+        v-for="show in showData"
+        :key="show"
+        @click="handleClickOpenModal(props.category, show)"
+      >
+        <div
+          :style="getImage(show)"
+          class="border-2 border-primary bg-cover bg-center w-[100px] h-[100px] rounded-full"
+        ></div>
         <div class="text-xs font-normal pt-2">{{ show.showStartTime }}</div>
         <div class="text-primary-700 font-medium">{{ show.performer }}</div>
       </div>
     </div>
   </div>
-  <div v-else class="w-full h-[178px] bg-white rounded-3xl border-primary border-1 flex justify-center select-none" v-bind="$attrs">
+  <div
+    v-else
+    class="w-full h-[178px] bg-white rounded-3xl border-primary border-1 flex justify-center select-none"
+    v-bind="$attrs"
+  >
     <div class="flex flex-col items-center pt-[18px] w-full px-3 gap-[17px] justify-evenly">
       <div class="gap-1 flex flex-col items-center">
         <div class="text-secondary-700 font-bold">현재 공연정보가 없습니다.</div>
