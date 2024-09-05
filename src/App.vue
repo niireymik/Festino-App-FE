5a70c43d816539346838d75bbe91e5b85cfdf1de
kim-song-jun <>
2024-09-05 13:20:50 +0900

    feat : add message

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,23 @@ import { onMounted } from 'vue';
 
 import ModalView from './views/ModalView.vue';
 
+import Swal from 'sweetalert2';
+
 onMounted(() => {
   document.documentElement.scrollTop = 0;
   document.body.scrollTop = 0;
+
+  Swal.fire({
+    width: '100%',
+    icon: 'warning',
+    title: '주의사항',
+    html: `
+      <div class="text-wrap break-words whitespace-pre-wrap">
+        <b>Festino</b>는 한국공학대학교 개발 소모임이 자체적으로 만든, 학생들을 위한 축제 정보/편의 제공 서비스입니다. \n\n 따라서, <b>총학생회와 무관</b>하며 정보가 정확하지 않을 수 있습니다.
+      </div>`,
+    confirmButtonText: '확인했습니다.',
+    footer: '<a href="https://www.instagram.com/tukorea_25_wind/" class="text-blue-400">총학생회 인스타 바로가기</a>',
+  });
 });
 </script>
 
--- src/components/timetable/TimeTable.vue
+++ src/components/timetable/TimeTable.vue
@@ -11,49 +11,52 @@ const { timetableData, day } = storeToRefs(useTimetableStore());
 const activateTimetable = ref(false);
 
 const handleClickOpenClubModal = (show) => {
-  const category = "club";
+  const category = 'club';
   handleClickOpenModal(category, show);
 };
 
 const handleClickOpenTalentModal = (show) => {
-  const category = "talent";
+  const category = 'talent';
   handleClickOpenModal(category, show);
 };
 
 const isShowingTime = (isShowing) => {
-  if(isShowing) return 'text-secondary-700';
+  if (isShowing) return 'text-secondary-700';
   else return 'text-secondary-100';
 };
 
 const isShowingPin = (isShowing) => {
-  if(isShowing) return 'bg-primary-700';
+  if (isShowing) return 'bg-primary-700';
   else return 'bg-secondary-100';
 };
 
 const isShowingBgPin = (isShowing) => {
-  if(isShowing) return 'bg-primary-700-light';
+  if (isShowing) return 'bg-primary-700-light';
   else return 'bg-secondary-50';
 };
 
 const updateActiveTimetable = () => {
   activateTimetable.value = false;
-  if(timetableData.value) {
+  if (timetableData.value) {
     timetableData.value.forEach((data) => {
-      if(data.isShowing) activateTimetable.value = true;
-      if(activateTimetable.value) data.isShowing = true;
+      if (data.isShowing) activateTimetable.value = true;
+      if (activateTimetable.value) data.isShowing = true;
     });
     if (!activateTimetable.value) {
       timetableData.value.forEach((data) => {
-      data.isShowing = true;
+        data.isShowing = true;
       });
     }
   }
 };
 
-watch(() => day.value, async () => {
-  await getAllTimetable();
-  updateActiveTimetable();
-});
+watch(
+  () => day.value,
+  async () => {
+    await getAllTimetable();
+    updateActiveTimetable();
+  },
+);
 
 onMounted(async () => {
   await getAllTimetable();
@@ -64,8 +67,11 @@ onMounted(async () => {
 <template>
   <div class="w-full select-none">
     <div class="flex flex-col items-center border-1 border-primary rounded-3xl py-5 shadow-4xl">
+      <div class="text-gray-400 text-2xs pb-2">* 주체측의 사정에 따라 일정이 달라질 수 있습니다.</div>
       <div class="px-5 pb-5">
-        <div class="w-[300px] xs:w-[350px] sm:w-[390px] py-2 text-white bg-primary-700 rounded-full flex justify-center">
+        <div
+          class="w-[300px] xs:w-[350px] sm:w-[390px] py-2 text-white bg-primary-700 rounded-full flex justify-center"
+        >
           DAY {{ day }} 공연 타임테이블
         </div>
       </div>
@@ -78,23 +84,36 @@ onMounted(async () => {
       </div>
       <div class="flex h-full w-full justify-center">
         <div class="flex flex-col items-center text-secondary-700 gap-[65px] pt-1">
-          <div v-for="data in timetableData" :key="data" :class="isShowingTime(data.isShowing)">{{ data.showStartTime }} ~ {{ data.showEndTime }}</div>
+          <div v-for="data in timetableData" :key="data" :class="isShowingTime(data.isShowing)">
+            {{ data.showStartTime }} ~ {{ data.showEndTime }}
+          </div>
         </div>
         <div v-if="timetableData && timetableData.length != 0" class="pt-3 pl-4 sm:pl-7 pr-3 xs:pr-4 sm:pr-7">
           <div class="border-2 border-primary-700 h-[auto] w-0 border-dashed flex flex-col items-center gap-[82.7px]">
-            <div class="w-[16px] h-[16px] mt-[-5px] mb-[-5px] rounded-full flex items-center justify-center" :class="isShowingBgPin(data.isShowing)" v-for="data in timetableData" :key="data">
+            <div
+              class="w-[16px] h-[16px] mt-[-5px] mb-[-5px] rounded-full flex items-center justify-center"
+              :class="isShowingBgPin(data.isShowing)"
+              v-for="data in timetableData"
+              :key="data"
+            >
               <div class="w-[8px] h-[8px] rounded-full" :class="isShowingPin(data.isShowing)"></div>
             </div>
           </div>
         </div>
         <div class="flex flex-col items-center gap-6">
           <div v-for="data in timetableData" :key="data">
-            <TimeTableDetail @click="handleClickOpenClubModal(data)" v-if="data.clubId"
+            <TimeTableDetail
+              @click="handleClickOpenClubModal(data)"
+              v-if="data.clubId"
               :data="data"
-              category="교내 동아리"/>
-            <TimeTableDetail @click="handleClickOpenTalentModal(data)" v-if="data.talentId"
+              category="교내 동아리"
+            />
+            <TimeTableDetail
+              @click="handleClickOpenTalentModal(data)"
+              v-if="data.talentId"
               :data="data"
-              category="연예인"/>
+              category="연예인"
+            />
           </div>
         </div>
       </div>
@@ -102,5 +121,4 @@ onMounted(async () => {
   </div>
 </template>
 
-<style lang="css" scoped>
-</style>
+<style lang="css" scoped></style>
