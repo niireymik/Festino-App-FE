6c6ea14206aea382fd891e46e118b1ef967270d0
jzongyun <jeong.yun@tukorea.ac.kr>
2024-09-07 15:23:27 +0900

    style : change timetable layout

--- src/components/timetable/TimeTable.vue
+++ src/components/timetable/TimeTable.vue
@@ -1,24 +1,12 @@
 <script setup>
 import { onMounted, watch, ref } from 'vue';
-import { useModalStore } from '@/stores/modalStore.js';
 import { useTimetableStore } from '@/stores/timetableStore.js';
 import { storeToRefs } from 'pinia';
 import TimeTableDetail from './TimeTableDetail.vue';
 
-const { handleClickOpenModal } = useModalStore();
-const { getAllTimetable } = useTimetableStore();
-const { timetableData, day } = storeToRefs(useTimetableStore());
+const { getClubTimetable } = useTimetableStore();
+const { clubData, day } = storeToRefs(useTimetableStore());
 const activateTimetable = ref(false);
-
-const handleClickOpenClubModal = (show) => {
-  const category = "club";
-  handleClickOpenModal(category, show);
-};
-
-const handleClickOpenTalentModal = (show) => {
-  const category = "talent";
-  handleClickOpenModal(category, show);
-};
 
 const isShowingTime = (isShowing) => {
   if(isShowing) return 'text-secondary-700';
@@ -37,39 +25,43 @@ const isShowingBgPin = (isShowing) => {
 
 const updateActiveTimetable = () => {
   activateTimetable.value = false;
-  if(timetableData.value) {
-    timetableData.value.forEach((data) => {
+  if(clubData.value.showInfo) {
+    clubData.value.showInfo.forEach((data) => {
       if(data.isShowing) activateTimetable.value = true;
       if(activateTimetable.value) data.isShowing = true;
     });
     if (!activateTimetable.value) {
-      timetableData.value.forEach((data) => {
+      clubData.value.showInfo.forEach((data) => {
       data.isShowing = true;
       });
     }
   }
 };
 
+const handleClickInstagram = () => {
+  window.open(`https://www.instagram.com/tukorea_25_wind/`, '_blank');
+};
+
 watch(() => day.value, async () => {
-  await getAllTimetable();
+  await getClubTimetable();
   updateActiveTimetable();
 });
 
 onMounted(async () => {
-  await getAllTimetable();
+  await getClubTimetable();
   updateActiveTimetable();
 });
 </script>
 
 <template>
   <div class="w-full select-none">
-    <div class="flex flex-col items-center border-1 border-primary rounded-3xl py-5 shadow-4xl">
+    <div class="flex flex-col items-center border-1 border-primary rounded-3xl py-5 shadow-4xl gap-4">
       <div class="px-5 pb-5">
         <div class="w-[300px] xs:w-[350px] sm:w-[390px] py-2 text-white bg-primary-700 rounded-full flex justify-center">
           DAY {{ day }} 공연 타임테이블
         </div>
       </div>
-      <div v-if="!timetableData || timetableData.length == 0" class="w-full flex flex-col items-center pb-4">
+      <div v-if="!clubData.showInfo || clubData.showInfo.length == 0" class="w-full flex flex-col items-center pb-4">
         <div class="pt-6 pb-[48px] gap-1 flex flex-col items-center">
           <div class="text-secondary-700 font-bold">현재 공연정보가 없습니다.</div>
           <div class="text-secondary-500 text-xs">추후 업데이트 예정입니다.</div>
@@ -77,26 +69,25 @@ onMounted(async () => {
         <div class="bg-tino-error-timetable bg-cover bg-center bg-no-repeat w-full aspect-[35/26]"></div>
       </div>
       <div class="flex h-full w-full justify-center">
-        <div class="flex flex-col items-center text-secondary-700 gap-[65px] pt-1">
-          <div v-for="data in timetableData" :key="data" :class="isShowingTime(data.isShowing)">{{ data.showStartTime }} ~ {{ data.showEndTime }}</div>
+        <div class="flex flex-col items-center text-secondary-700 gap-[162px] pt-1 mt-[-9px]">
+          <div v-for="data in clubData.showInfo" :key="data" :class="isShowingTime(data.isShowing)">{{ data.showStartTime }} ~ {{ data.showEndTime }}</div>
         </div>
-        <div v-if="timetableData && timetableData.length != 0" class="pt-3 pl-4 sm:pl-7 pr-3 xs:pr-4 sm:pr-7">
-          <div class="border-2 border-primary-700 h-[auto] w-0 border-dashed flex flex-col items-center gap-[82.7px]">
-            <div class="w-[16px] h-[16px] mt-[-5px] mb-[-5px] rounded-full flex items-center justify-center" :class="isShowingBgPin(data.isShowing)" v-for="data in timetableData" :key="data">
+        <div v-if="clubData.showInfo && clubData.showInfo.length != 0" class="pt-3 pl-4 sm:pl-7 pr-3 xs:pr-4 sm:pr-7">
+          <div class="border-2 border-primary-700 h-[auto] w-0 border-dashed flex flex-col items-center gap-44 pb-32 mt-[-10px]">
+            <div class="w-[16px] h-[16px] mt-[-5px] rounded-full flex items-center justify-center" :class="isShowingBgPin(data.isShowing)" v-for="data in clubData.showInfo" :key="data">
               <div class="w-[8px] h-[8px] rounded-full" :class="isShowingPin(data.isShowing)"></div>
             </div>
           </div>
         </div>
         <div class="flex flex-col items-center gap-6">
-          <div v-for="data in timetableData" :key="data">
-            <TimeTableDetail @click="handleClickOpenClubModal(data)" v-if="data.clubId"
-              :data="data"
-              category="교내 동아리"/>
-            <TimeTableDetail @click="handleClickOpenTalentModal(data)" v-if="data.talentId"
-              :data="data"
-              category="연예인"/>
+          <div v-for="data in clubData.showInfo" :key="data">
+            <TimeTableDetail :data="data" />
           </div>
         </div>
+      </div>
+      <div class="text-center flex flex-col gap-2">
+        <p class="text-primary-700">자세한 공연 정보가 궁금하다면?</p>
+        <button @click="handleClickInstagram()" class="text-white w-[256px] h-[30px] rounded-full bg-primary-700">총학생회 wind 인스타그램 바로가기</button>
       </div>
     </div>
   </div>
--- src/components/timetable/TimeTableDetail.vue
+++ src/components/timetable/TimeTableDetail.vue
@@ -1,24 +1,40 @@
 <script setup>
-  const props = defineProps({
+import { useModalStore } from '@/stores/modalStore.js';
+
+const { handleClickOpenModal } = useModalStore();
+
+const props = defineProps({
   data: {
     type: Object,
     required: true
   },
-  category: {
-    type: String,
-    required: true
-  },
 });
+
+const handleClickOpenClubModal = () => {
+  handleClickOpenModal(props.data);
+};
 
 const isShowing = (isShowing) => {
   if(isShowing) return 'border-primary text-primary-700'
   else return 'bg-secondary-50 border-secondary-100 text-secondary-100'
 };
+
+const getImage = () => {
+  return { backgroundImage: `url('${props.data.clubImage}')` };
+};
 </script>
 
 <template>
-  <div class="cursor-pointer flex justify-center py-5 rounded-3xl w-[170px] xs:w-[210px] sm:w-[230px] border-2"
-    :class="isShowing(data.isShowing)">{{ category }} ' {{ data.performer }} '</div>
+  <div class="cursor-pointer flex py-5 rounded-3xl w-[170px] xs:w-[210px] sm:w-[230px] border-2 flex-col items-center gap-2.5 shadow-4xl "
+    :class="isShowing(data.isShowing)">
+    <div class="rounded-full bg-primary-700 w-9 h-9 border-2 border-primary bg-cover bg-center"
+    :style="getImage()"></div>
+    <div class="text-center leading-none">
+      <div>교내 동아리 공연</div>
+      <div>' {{ data.performer }} '</div>
+    </div>
+    <button @click="handleClickOpenClubModal()" class="text-white bg-primary-700 w-[152px] h-[30px] rounded-full">공연 정보 상세보기</button>
+  </div>
 </template>
 
 <style lang="css" scoped>
