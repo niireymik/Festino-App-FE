3e70292b3e73fa27ad793b81b29baa9ce65a92e4
jzongyun <jeong.yun@tukorea.ac.kr>
2024-09-08 15:17:37 +0900

    style : change logo size in small screen

--- src/components/timetable/ShowPreview.vue
+++ src/components/timetable/ShowPreview.vue
@@ -30,17 +30,17 @@ onMounted(async () => {
 
 <template>
   <div
-    class="w-full h-[178px] bg-white rounded-3xl border-primary border-1 flex justify-center select-none"
+    class="w-full h-[160px] sm:h-[178px] bg-white rounded-3xl border-primary border-1 flex justify-center select-none"
     v-bind="$attrs"
   >
     <div class="flex pt-[17px] w-full px-3 justify-evenly gap-1 overflow-x-auto reserve-container">
       <div class="flex flex-col items-center cursor-pointer" v-for="show in showData" :key="show" @click="handleClickOpenModal(show)">
-        <div :style="getImage(show)" class="border-2 border-primary bg-cover bg-center w-[100px] h-[100px] rounded-full"></div>
+        <div :style="getImage(show)" class="border-2 border-primary bg-cover bg-center w-[86px] h-[86px] sm:w-[100px] sm:h-[100px] rounded-full"></div>
         <div class="text-xs font-normal pt-2">{{ show.showStartTime }}</div>
         <div class="text-primary-700 font-medium">{{ show.performer }}</div>
       </div>
       <div class="flex flex-col items-center cursor-pointer" @click="handleClickInstagram()">
-        <div class="border-2 border-primary bg-cover bg-center bg-talent-icon w-[100px] h-[100px] rounded-full"></div>
+        <div class="border-2 border-primary bg-cover bg-center bg-talent-icon w-[86px] h-[86px] sm:w-[100px] sm:h-[100px] rounded-full"></div>
         <div class="text-primary-700 font-medium pt-6">연예인 공연</div>
       </div>
     </div>
