9453671ff04797b9185aaf561ceb5b9e9cb7a11b
kim-song-jun <>
2024-09-09 11:52:51 +0900

    feat : change open date

--- src/components/tablings/modals/FailReservationModal.vue
+++ src/components/tablings/modals/FailReservationModal.vue
@@ -2,6 +2,21 @@
 import { useBaseModal } from '@/stores/baseModal';
 
 const { closeModal } = useBaseModal();
+
+// Get today, and if today is 2024 9/11 ~ 9/13, return true or return false
+// and Change to KST
+const getIsOpenDay = () => {
+  const today = new Date();
+  today.setHours(today.getHours() + 9);
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1;
+  const date = today.getDate();
+
+  if (year === 2024 && month === 9 && date >= 11 && date <= 13) {
+    return true;
+  }
+  return false;
+};
 </script>
 
 <template>
@@ -14,7 +29,8 @@ const { closeModal } = useBaseModal();
     </div>
     <div class="w-full flex flex-col gap-3 items-center">
       <p class="text-secondary-700 text-xl font-bold">예약실패</p>
-      <p class="text-secondary-500">이미 예약이 존재합니다.</p>
+      <p v-if="getIsOpenDay()" class="text-secondary-500">이미 예약이 존재합니다.</p>
+      <p v-if="!getIsOpenDay()" class="text-secondary-500">9월 11일 부터 예약이 가능합니다.</p>
     </div>
     <button class="w-full h-12 bg-primary-900 rounded-3xl text-white font-semibold text-xl" @click="closeModal()">
       확인
