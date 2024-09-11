e951330f834ccb589318088c7e9e7ba132b5c3f3
heedong12 <dlgmldus3289@naver.com>
2024-09-11 17:50:56 +0900

    fix: change crying tino

--- src/components/tablings/NoBooth.vue
+++ src/components/tablings/NoBooth.vue
@@ -2,7 +2,7 @@
 <template>
   <div class="w-full flex flex-col items-center gap-2 py-2">
     <p class="text-xl font-medium">예약 가능한 부스가 없습니다.</p>
-    <img src="/icons/tino-error.svg" class="w-1/2" />
+    <div class="bg-tino-error-timetable bg-cover bg-center bg-no-repeat w-full aspect-[35/26]"></div>
   </div>
 </template>
 
